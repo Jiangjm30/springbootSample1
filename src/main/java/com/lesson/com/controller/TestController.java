83a24574f66a711d307318c0f1bd23ab626810c7
jiangjm30 <aimutjmspe@gmail.com>
2021-07-30 18:43:55 +0900

    7/30 lesson2

--- src/main/java/com/lesson/com/controller/TestController.java
+++ src/main/java/com/lesson/com/controller/TestController.java
@@ -1,7 +1,33 @@
 package com.lesson.com.controller;
 
 import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.web.bind.annotation.*;
 
-@SpringBootConfiguration
+@RestController
 public class TestController {
+
+    @GetMapping("/hello")
+    public String test1(){
+        return "hello ";
+    }
+    @PostMapping ("/hello2")
+    public String test2(){
+        return "helloPost ";
+    }
+    @DeleteMapping  ("/hello3")
+    public String test3(){
+        return "helloDelete ";
+    }
+    @GetMapping("/param/{abc}/{decf}")
+    public String test4(@PathVariable String abc,@PathVariable String decf){
+        return "hello " + abc + " : " + decf;
+    }
+    @GetMapping("/param/{userId}/{passwd}")
+    public String test5(@PathVariable("userid") String abc,@PathVariable("passwd") String decf){
+        return "hello " + abc + " : " + decf;
+    }
+    @GetMapping("/param/{userId}/{passwd}")
+    public String test6(@PathVariable("userid") String abc,@PathVariable("decf") String passwd){
+        return "hello " + abc + " : " + passwd;
+    }
 }
